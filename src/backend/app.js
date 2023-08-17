4f477e16aa73fc64f544ed4a254cb838a87bb328
Peter Rosellen <2063341+prosellen@users.noreply.github.com>
2023-08-17 22:37:39 +0200

    Adds cors, `/api/`endpoint, and lists permissions in the response

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,19 +1,21 @@
+const cors = require("cors");
 const createError = require("http-errors");
 const express = require("express");
 
 const indexRouter = require("./routes/index");
+const apiRouter = require("./routes/api");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
+app.use("/api/", apiRouter);
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
+app.use((req, res, next) => next(createError(404)));
 
 // error handler
 app.use((err, req, res) => {
--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -1,31 +1,12 @@
 const express = require("express");
-const jwtDecode = require("jwt-decode");
 
 const router = express.Router();
 
 // A route that should be accessible for everyone
 router.get("/", (req, res) =>
   res.status(200).json({
-    message: "This is an unprotected endpoint that is available to everyone.",
+    message: "This is the root-route. Nothing to see here.",
   }),
 );
-
-// A route that should only be accessible for users with a valid access token
-router.get("/protected/", (req, res) => {
-  let scopesArray = [];
-  const authHeader = req.header("authorization");
-
-  if (authHeader) {
-    const jwtToken = authHeader.split(" ")[1]; // Header ist "Bearer eyJhbGciOi..." and this will get the last part, which is the JWT
-    const jwtPayload = jwtDecode(jwtToken); // This Node Package returns the decoded payload from the JWT Token
-    scopesArray = jwtPayload?.scope.split(" "); // The scope is space separated string
-  }
-
-  return res.status(200).json({
-    message:
-      "This is a secret message that only authorized users should be able to get from the API.",
-    availableScopes: scopesArray,
-  });
-});
 
 module.exports = router;
